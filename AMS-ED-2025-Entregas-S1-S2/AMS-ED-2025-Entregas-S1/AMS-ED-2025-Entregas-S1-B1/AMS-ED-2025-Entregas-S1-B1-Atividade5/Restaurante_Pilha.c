24e9f7ef833407fcdec513bcda2f7e6584d0e76b
Milena Sawada <miwaa.sawada@gmail.com>
2025-04-02 11:34:57 -0300

    Update Restaurante_Pilha.c

--- AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade5/Restaurante_Pilha.c
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade5/Restaurante_Pilha.c
@@ -3,7 +3,7 @@
 /*                         Id da Atividade: Atividade B1-5                          */
 /*          Objetivo: Programa de Restaurante - transformando lista ligada em pilha */
 /*                                                                                  */
-/*                                  Autores: Isabella Monsalles Barbosa             */
+/*                                  Autoras: Isabella Monsalles Barbosa             */
 /*                                           Milena Miwa Sawada                     */
 /*                                                                   Data:01/04/2025*/
 /*----------------------------------------------------------------------------------*/
